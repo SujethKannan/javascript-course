5aa295536d60c9b5ea1a0a203f57f4f4577faa50
SujethKannan <sujethkannan98@gmail.com>
2023-05-25 20:16:04 +0530

    commit-3

---
+++ declaration_type/declaration.js
@@ -0,0 +1,79 @@
+var a=1
+console.log(a)
+b=5
+console.log(b)
+c='sujeth'
+console.log(c)
+d=3+27
+console.log(d)
+let r=43
+console.log(r)
+f= 'my first coding class'
+console.log(f)
+const num=45
+console.log(num)
+// console.log(r)
+var sk='global variable1'
+kl='global variable2'
+function hello() {
+    let uv='local variable1'
+    xy='local variable2'
+    console.log(uv)
+    console.log(xy)
+}
+console.log(sk)
+console.log(kl)
+var o
+console.log(o)
+let y
+console.log(y)
+const p=4
+console.log(p)
+let q=3/0
+console.log(q)
+let w=0/9
+console.log(w)
+let qw=8/9
+console.log(qw)
+let er=36+96
+console.log(er)
+let ty=56-89
+console.log(ty)
+let ui= 3+'sujeth'
+console.log(ui)
+let jk= 7+true
+console.log(jk)
+let kj=7+false
+console.log(kj)
+let oi=String(55)+' java'
+console.log(oi)
+var z=3/0
+console.log(z)
+var x=0/9
+console.log(x)
+var xz=8/9
+console.log(xz)
+var re=36+96
+console.log(re)
+var yt=56-89
+console.log(yt)
+var iu= 3+'sujeth'
+console.log(iu)
+var mk= 7+true
+console.log(mk)
+var km=7+false
+console.log(km)
+var io=String(55)+' java'
+console.log(io)
+var po='sk'+ undefined
+console.log(po)
+var op=7+undefined
+console.log(op)
+var uj=true+undefined
+console.log(uj)
+const array = [1,2,3,4,'sujeth',true,undefined]
+console.log(array)
+//const array = new Array(1,2,3,4,'sujeth',true,undefined)
+//console.log(array)
+console.log(array.length)
+console.log(array[4])
---
+++ loop/loop.js
@@ -0,0 +1,15 @@
+//for(let i=0;i<10;i++){
+  //  console.log(i);
+//}
+const items = [9,8,7,4,5,6]
+//for(let i=0;i<items.length;i++){
+  //  console.log(items[i]);
+//}
+//items.forEach(e=>{
+  //  console.log(e)})
+  //for (let a of items){
+    //console.log(a)
+  //}
+  for (let a in items){
+    console.log(a)
+  }
